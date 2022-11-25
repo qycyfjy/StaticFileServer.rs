b74ef7ccad955a2d83d332de8d7b04ad9a11e362
kyaru <kyaruny@gmail.com>
2022-11-26 06:53:14 +0800

    v0.0.2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![windows_subsystem = "windows"]
+// #![windows_subsystem = "windows"]
 
 extern crate native_windows_derive as nwd;
 extern crate native_windows_gui as nwg;
@@ -6,61 +6,15 @@ extern crate native_windows_gui as nwg;
 use nwd::NwgUi;
 use nwg::NativeUi;
 use rocket::fs::FileServer;
+use walkdir::WalkDir;
 
 use std::{
     cell::{Cell, RefCell},
     path,
 };
 
-fn start_server(
-    path: String,
-) -> (
-    rocket::tokio::sync::oneshot::Sender<bool>,
-    std::thread::JoinHandle<()>,
-) {
-    let (tx, rx) = rocket::tokio::sync::oneshot::channel();
-    let t = std::thread::spawn(move || {
-        let path = path;
-        rocket::tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap()
-            .block_on(async move {
-                let config = rocket::Config {
-                    port: 9234,
-                    address: std::net::Ipv4Addr::new(127, 0, 0, 1).into(),
-                    ..Default::default()
-                };
-                let mut rocket = rocket::build()
-                    .configure(config)
-                    .mount("/", FileServer::from(&path));
-                if !find_index(&path) {
-                    rocket = rocket.mount("/", rocket::routes![dir_list]);
-                }
-                let rocket = rocket.launch();
-                rocket::tokio::select! {
-                    _ = rx => {
-                        println!("Server Stop...");
-                    }
-                    _ = rocket => {
-
-                    }
-                }
-            })
-    });
-    (tx, t)
-}
-
-#[rocket::get("/")]
-fn dir_list() -> String {
-    let mut result = "<pre>\n".to_string();
-
-    result.push_str("</pre>\n");
-    result
-}
-
-fn find_index(root: &str) -> bool {
-    let path = std::path::Path::new(root);
+fn find_index(path: &str) -> bool {
+    let path = std::path::Path::new(path);
     for entry in path.read_dir().unwrap() {
         if let Ok(entry) = entry {
             if entry.path().file_name().unwrap() == "index.html" {
@@ -70,6 +24,105 @@ fn find_index(root: &str) -> bool {
     }
     false
 }
+
+fn dir_list_html(path: &str) -> String {
+    let mut r = "<pre>\n".to_string();
+    for file in WalkDir::new(path)
+        .follow_links(false)
+        .into_iter()
+        .filter_map(|file| file.ok())
+    {
+        if file.metadata().unwrap().is_file() {
+            let fullpath = file.into_path();
+            if let Some(trimmed) = pathdiff::diff_paths(fullpath, path) {
+                let trimmed = trimmed.into_os_string().into_string().unwrap();
+                let line = format!(
+                    "<a href=\"http://127.0.0.1:9234/{}\">{}</a>\n",
+                    trimmed, trimmed
+                );
+                r.push_str(&line);
+            }
+        }
+    }
+    r.push_str("</pre>\n");
+    r
+}
+
+struct Model {
+    dir_list_html: String,
+}
+
+impl Model {
+    fn new(html: String) -> Self {
+        Model {
+            dir_list_html: html,
+        }
+    }
+
+    fn get(&self) -> String {
+        self.dir_list_html.clone()
+    }
+}
+
+#[rocket::get("/")]
+fn dir_list(model: &rocket::State<Model>) -> rocket::response::content::RawHtml<String> {
+    let stuff = model.get();
+    rocket::response::content::RawHtml(stuff)
+}
+
+struct StaticFileServer {
+    root: String,
+}
+
+impl StaticFileServer {
+    pub fn new(path: String) -> Self {
+        StaticFileServer { root: path }
+    }
+
+    pub fn start(
+        &self,
+    ) -> (
+        rocket::tokio::sync::oneshot::Sender<bool>,
+        std::thread::JoinHandle<()>,
+    ) {
+        let (tx, rx) = rocket::tokio::sync::oneshot::channel();
+        let root = self.root.clone();
+        let t = std::thread::spawn(move || {
+            rocket::tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .unwrap()
+                .block_on(async move {
+                    let config = rocket::Config {
+                        port: 9234,
+                        address: std::net::Ipv4Addr::new(127, 0, 0, 1).into(),
+                        ..Default::default()
+                    };
+                    let mut rocket = rocket::build()
+                        .configure(config)
+                        .mount("/", FileServer::from(&root));
+
+                    if !find_index(&root) {
+                        let model = Model::new(dir_list_html(&root));
+                        rocket = rocket.manage(model).mount("/", rocket::routes![dir_list]);
+                    }
+
+                    let rocket = rocket.launch();
+                    rocket::tokio::select! {
+                        _ = rx => {
+                            println!("Server Stop...");
+                        }
+                        _ = rocket => {
+
+                        }
+                    }
+                })
+        });
+        (tx, t)
+    }
+}
+
+impl StaticFileServer {}
 
 #[derive(Default, NwgUi)]
 pub struct StaticFileServerApp {
@@ -129,7 +182,8 @@ impl StaticFileServerApp {
             self.start_button.set_text("Start");
         } else {
             let path = self.path.borrow().clone().unwrap();
-            let (tx, t) = start_server(path);
+            let server = StaticFileServer::new(path);
+            let (tx, t) = server.start();
             self.server_thread.replace(Some(t));
             self.shutdown.replace(Some(tx));
             self.start_button
